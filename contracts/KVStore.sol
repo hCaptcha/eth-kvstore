e2c6ca80c6e1b3a19d603b86a72f61a163272d12
Robert Valta <robert.j.valta@gmail.com>
2018-08-19 19:19:38 +0300

    Simplify the contract by removing authorization functionality

--- contracts/KVStore.sol
+++ contracts/KVStore.sol
@@ -3,31 +3,10 @@ pragma solidity ^0.4.24;
 
 contract KVStore {
     
-    enum AccessRight { Revoked, Invoked }
     uint constant private MAX_STRING_LENGTH = 1000;
-
     mapping(address => mapping(string => string)) private store;
-    mapping(address => mapping(address => uint)) private authorizedAccounts;
-
-    constructor() public {
-        authorizeAccount(msg.sender);
-    }
-
-    function authorizeAccount(address _account) public {
-        authorizedAccounts[_account][msg.sender] = uint(AccessRight.Invoked);
-    }
-
-    function revokeAccount(address _account) public {
-        require(_account != msg.sender);
-        authorizedAccounts[_account][msg.sender] = uint(AccessRight.Revoked);
-    }
-
-    function isAuthorized(address _account) public view returns(uint) {
-        return authorizedAccounts[msg.sender][_account];
-    }
 
     function get(address _account, string _key) public view returns(string) {
-        require(isAuthorized(_account) == uint(AccessRight.Invoked));
         return store[_account][_key];
     }
 
