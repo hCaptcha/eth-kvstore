f0bc4d2228f9bed15ddac691873bae3fc68edb3b
Robert Valta <robert.j.valta@gmail.com>
2018-08-19 19:20:00 +0300

    Adjust tests to new changes

--- test/kvstore.js
+++ test/kvstore.js
@@ -1,113 +1,46 @@
 const KVStore = artifacts.require('./KVStore.sol');
 
-/**
- * We set up the enums AccessRight.Invoked and AccessRight.Revoked to map authorized users.
- * AccessRight.Invoked = 1
- * AccessRight.Revoked = 0
- */
-
 contract('KVStore', async (accounts) => {
-  it('adds the account itself to authorized accounts', async () => {
+  it('returns a correct value to the address storing the key-value pair', async () => {
     const instance = await KVStore.deployed();
     const [accountOne] = accounts;
-    const authorizedAccount = await instance.isAuthorized(accountOne, {
+    await instance.set('satoshi', 'nakamoto', {
       from: accountOne,
     });
-    assert.equal(authorizedAccount, 1);
-  });
-
-  it('adds an account to authorized account', async () => {
-    const instance = await KVStore.deployed();
-    const [accountOne, accountTwo] = accounts;
-    await instance.authorizeAccount(accountTwo, { from: accountOne });
-    const authorizedAccount = await instance.isAuthorized(accountOne, {
-      from: accountTwo,
-    });
-    assert.equal(authorizedAccount, 1);
-  });
-
-  it('denies the account revoking itself from authorized accounts', async () => {
-    const instance = await KVStore.deployed();
-    const [accountOne] = accounts;
-    try {
-      await instance.revokeAccount(accountOne, { from: accountOne });
-      assert.fail();
-    } catch (err) {
-      assert.ok(/revert/.test(err.message));
-    }
-  });
-
-  it('removes an account from authorized accounts', async () => {
-    const instance = await KVStore.deployed();
-    const [accountOne, accountTwo] = accounts;
-    await instance.authorizeAccount(accountTwo, { from: accountOne });
-    let authorizedAccount = await instance.isAuthorized(accountOne, {
-      from: accountTwo,
-    });
-    assert.equal(authorizedAccount, 1);
-
-    await instance.revokeAccount(accountTwo, {
-      from: accountOne,
-    });
-    authorizedAccount = await instance.isAuthorized(accountOne, {
-      from: accountTwo,
-    });
-    assert.equal(authorizedAccount, 0);
-  });
-
-  it('returns the correct value to the account itself', async () => {
-    const instance = await KVStore.deployed();
-    const [accountOne] = accounts;
-    await instance.set('satoshi', 'nakamoto', { from: accountOne });
     const value = await instance.get.call(accountOne, 'satoshi', {
       from: accountOne,
     });
     assert.equal(value, 'nakamoto');
   });
 
-  it('returns the correct value to an authorized account', async () => {
+  it('returns a correct value to another address', async () => {
     const instance = await KVStore.deployed();
     const [accountOne, accountTwo] = accounts;
-    await instance.authorizeAccount(accountTwo, { from: accountOne });
-    await instance.set('satoshi', 'nakamoto', { from: accountOne });
+    await instance.set('satoshi', 'nakamoto', {
+      from: accountOne,
+    });
     const value = await instance.get.call(accountOne, 'satoshi', {
       from: accountTwo,
     });
     assert.equal(value, 'nakamoto');
   });
 
-  it("doesn't return a value to an unauthorized account", async () => {
+  it("doesn't allow storing a too long key", async () => {
     const instance = await KVStore.deployed();
-    const [accountOne, accountTwo, accountThree] = accounts;
-    await instance.authorizeAccount(accountTwo, { from: accountOne });
-    await instance.set('satoshi', 'nakamoto', { from: accountOne });
+    const [accountOne] = accounts;
     try {
-      await instance.get.call(accountOne, 'satoshi', {
-        from: accountThree,
-      });
+      await instance.set(
+        'satoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamotosatoshinakamoto',
+        'satoshi',
+        { from: accountOne },
+      );
       assert.fail();
     } catch (err) {
       assert.ok(/revert/.test(err.message));
     }
   });
 
-  it("doesn't return a value to a revoked account", async () => {
-    const instance = await KVStore.deployed();
-    const [accountOne, accountTwo] = accounts;
-    await instance.authorizeAccount(accountTwo, { from: accountOne });
-    await instance.revokeAccount(accountTwo, { from: accountOne });
-    await instance.set('satoshi', 'nakamoto', { from: accountOne });
-    try {
-      await instance.get.call(accountOne, 'satoshi', {
-        from: accountTwo,
-      });
-      assert.fail();
-    } catch (err) {
-      assert.ok(/revert/.test(err.message));
-    }
-  });
-
-  it("doesn't allow storing a too long string", async () => {
+  it("doesn't allow storing a too long value", async () => {
     const instance = await KVStore.deployed();
     const [accountOne] = accounts;
     try {
